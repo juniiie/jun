00abe5df79c25ae43008c45856ca051e232d23c1
Climson Jr <jrbuking@gmail.com>
2022-10-04 01:11:21 +1300

    Halfway completing

--- Documents/GitHub/crud-app/client/src/App.js
+++ Documents/GitHub/crud-app/client/src/App.js
@@ -1,7 +1,68 @@
 import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
-  return <div className="App">Hello Youtube</div>;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      console.log("success");
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        ></input>
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        ></input>
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        ></input>
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        ></input>
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
