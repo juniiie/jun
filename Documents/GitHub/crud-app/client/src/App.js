345dbda5d3e266555981216b294969eee64796a3
Climson Jr <jrbuking@gmail.com>
2022-10-06 23:42:42 +1300

    Completed CRUD React APP

--- Documents/GitHub/crud-app/client/src/App.js
+++ Documents/GitHub/crud-app/client/src/App.js
@@ -11,6 +11,11 @@ function App() {
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
 
+  const [newWage, setNewWage] = useState(0);
+
+  // Show stat employees
+  const [employeeList, setEmployeeList] = useState([]);
+
   const addEmployee = () => {
     // Send info from FE TO BE - Basically sending objects below to BE
     Axios.post("http://localhost:3001/create", {
@@ -21,7 +26,58 @@ function App() {
       wage: wage,
     }).then(() => {
       console.log("success");
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
     });
+  };
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  // Update employees
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
+      (response) => {
+        // alert("Update");
+        // Shows the new changes immediately
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id == id
+              ? {
+                  id: val.id,
+                  name: val.name,
+                  country: val.country,
+                  position: val.position,
+                  age: val.age,
+                  wage: newWage,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  // Delete employee
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => [
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id != id;
+        })
+      ),
+    ]);
   };
 
   return (
@@ -63,6 +119,55 @@ function App() {
           }}
         ></input>
         <button onClick={addEmployee}>Add Employee</button>
+      </div>
+
+      {/* Show eomplyees */}
+      <div className="employees">
+        <button onClick={getEmployees} className="employeeBtn">
+          Show Employees
+        </button>
+
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Position:{val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="2000.."
+                  onChange={(event) => {
+                    setNewWage(event.target.value);
+                  }}
+                ></input>
+
+                {/* Update button */}
+                <button
+                  onClick={() => {
+                    updateEmployeeWage(val.id);
+                  }}
+                  className="employeeBtn"
+                >
+                  Update
+                </button>
+
+                {/* Delete Button */}
+                <button
+                  onClick={() => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
